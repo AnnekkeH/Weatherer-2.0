b09dd1ef05a015f78da8397204bdf218c7c34cb2
Charlotte <58598938+Charlotta-16@users.noreply.github.com>
2023-11-30 15:27:12 -0700

    dunno

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 
 int main() {
     const auto weather_provider = std::make_unique<weather::WeatherProvider>("37.11713922134418,-113.61091493319164");
-    std::cout << weather_provider->get_two_week_forecast().dump(4) << "\n";
+    
+    //std::cout << weather_provider->get_two_week_forecast().dump(4) << "\n";
     return 0;
 }
