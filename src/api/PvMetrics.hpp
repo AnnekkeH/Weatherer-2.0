77a66b5fc2354b15a339e55cf85dbbd6a980599e
OneCheetah <91170949+OneCheetah@users.noreply.github.com>
2023-12-29 14:38:56 -0700

    added ability to calculate solar efficiency

---
+++ src/api/PvMetrics.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <cmath>
+
+#include "models/Coordinates.hpp"
+#include "models/PvData.hpp"
+
+namespace weatherer {
+class PvMetrics {
+ private:
+  static double CalculateSolarIrradiance(const PvData& pv_data);
+  static int CalculateSolarNoonTime(const PvData& pv_data);
+  static int GetDayOfYear(const PvData& pv_data);
+
+ public:
+  PvMetrics() = delete;
+  ~PvMetrics() = delete;
+  static double CalculateDailyEnergyYeild(PvData const& pv_data,
+                                          Coordinates const& coordinates,
+                                          std::string const& date,
+                                          const double panel_eff,
+                                          const double panel_area);
+};
+}  // namespace weatherer
