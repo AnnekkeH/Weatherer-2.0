1c0cea8af1c9cb60911093f319f5a4452efed56b
OneCheetah <91170949+OneCheetah@users.noreply.github.com>
2023-12-29 14:38:56 -0700

    ability to calculate solar panel output

---
+++ src/api/PvMetrics.cpp
@@ -0,0 +1,95 @@
+#include "PvMetrics.hpp"
+#include <cmath>
+#include <iostream>
+#include <numbers>
+#include <util/Statistics.hpp>
+#include <vector>
+#include "api/models/Coordinates.hpp"
+#include "util/Time.hpp"
+
+double weatherer::PvMetrics::CalculateSolarIrradiance(const PvData& pv_data) {
+  std::vector<double> hourly_sum(24);
+
+  // Fill the vector with sums of direct and diffuse radiation
+  std::iota(hourly_sum.begin(), hourly_sum.end(),
+            0);  // Just a placeholder; replace with actual values
+
+  // Transform the vector to contain the sum of direct and diffuse radiation for each hour
+  std::ranges::transform(hourly_sum.begin(), hourly_sum.end(),
+                         hourly_sum.begin(), [pv_data](const int hour) {
+                           return pv_data.GetDirectRadiation().at(hour) +
+                                  pv_data.GetDiffuseRadiation().at(hour);
+                         });
+
+  // Calculate the median of the hourly sums
+  const double overallMedian = util::Statistics::Mean(hourly_sum);
+  return overallMedian;
+}
+
+int weatherer::PvMetrics::CalculateSolarNoonTime(const PvData& pv_data) {
+  const auto sunrise_time = util::Time::ParseTime(pv_data.GetSunriseTime());
+  const auto sunset_time = util::Time::ParseTime(pv_data.GetSunsetTime());
+
+  const auto duration = sunset_time - sunrise_time;
+
+  // Calculate the midpoint duration
+  const auto midpoint_duration = duration / 2;
+
+  const auto solar_noon = sunrise_time + midpoint_duration;
+  const std::time_t solar_noon_time =
+      std::chrono::system_clock::to_time_t(solar_noon);
+  std::tm solar_noon_tm{};
+  localtime_s(&solar_noon_tm, &solar_noon_time);
+
+  return solar_noon_tm.tm_hour + 1;
+}
+
+double weatherer::PvMetrics::CalculateDailyEnergyYeild(
+    PvData const& pv_data, Coordinates const& coordinates,
+    std::string const& date, const double panel_eff, const double panel_area) {
+  const auto convert_to_radians = [](const double degrees) {
+    return degrees * (std::numbers::pi / 180);
+  };
+
+  const double average_temperature =
+      util::Statistics::Mean(pv_data.GetTemperature());
+  const double average_cloud_cover = static_cast<double>(util::Statistics::Mean(
+                                         pv_data.GetCloudCoverTotal())) /
+                                     100;
+
+  const double radians_latitude = convert_to_radians(coordinates.GetLatitude());
+  const double solar_irradiance = CalculateSolarIrradiance(pv_data);
+
+  const int solar_time = CalculateSolarNoonTime(pv_data);
+  const int day_of_year =
+      util::Time::ConvertTimePointToTm(util::Time::ParseDate(date)).tm_yday;
+
+  // Calculate solar declination angle (δ)
+  const double sollar_declination = 23.45 * sin((365 / 360) * (day_of_year - 81));
+  const double radians_sollar_declination =
+      convert_to_radians(sollar_declination);
+
+  // Calculate latitude factor
+  const double latitude_factor =
+      cos(radians_latitude) * cos(convert_to_radians(sollar_declination));
+
+  // Calculate temperature factor
+  const double temperature_factor = 1 - 0.005 * (average_temperature - 25);
+
+  // Calculate incident angle factor
+  const int hour_angle = 15 * (solar_time - 12);
+
+  // Calculate incident angle factor
+  const double solar_zentith_angle =
+      asin(sin(radians_latitude) * sin(radians_sollar_declination) +
+           cos(radians_latitude) * cos(radians_sollar_declination) *
+               cos(convert_to_radians(hour_angle)));
+  const double incidentAngleFactor = cos(solar_zentith_angle);
+
+  // Adjust irradiance with factors
+  const double adjusted_irradiance = solar_irradiance * latitude_factor *
+                                     temperature_factor * incidentAngleFactor *
+                                     (1 - average_cloud_cover);
+
+  return adjusted_irradiance * panel_eff * panel_area;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
-#include "api/PvHandler.hpp"
-#include <utility>
 #include <chrono>
 #include <iostream>
+#include <utility>
+#include "api/PvHandler.hpp"
+#include "api/PvMetrics.hpp"
+#include "api/models/Coordinates.hpp"
+#include "util/Time.hpp"
 
 int main() {
-	const weatherer::BulkPvDataPtr map =
-		weatherer::PvHandler::aggregate({52.52, 13.41}, "2023-12-01", "2023-12-18");
-	for (const auto& [key, value] : *map) {
-		std::cout << key << "\n" << *value << "\n";
-	}
+  const weatherer::Coordinates coordinates{52.52, 13.41};
+  const weatherer::BulkPvDataPtr map = weatherer::PvHandler::AggregateData(
+      coordinates, {"2023-12-01", "2023-12-18"});
+  for (const auto& [key, value] : *map) {
+    std::cout << key << "\n";
+    std::cout << weatherer::PvMetrics::CalculateDailyEnergyYeild(
+                     *value, coordinates, key, 0.95, 10)
+              << " kWh\n\n";
+  }
 }
