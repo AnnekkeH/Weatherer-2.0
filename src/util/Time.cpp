26fcf0bcef03b5c804ff99c43e7f06eee4c6ae7a
OneCheetah <91170949+OneCheetah@users.noreply.github.com>
2023-12-29 14:37:16 -0700

    added additional time utils functions

--- src/util/Time.cpp
+++ src/util/Time.cpp
@@ -3,117 +3,114 @@
 #include <array>
 #include <cmath>
 
-[[nodiscard]]
-weatherer::util::DateTimeInfo weatherer::util::Time::get_current_date() {
-	const std::chrono::time_point now = std::chrono::system_clock::now();
-	const std::chrono::time_point date = std::chrono::floor<std::chrono::days>(now);
-	const std::chrono::time_point midnight = std::chrono::time_point_cast<std::chrono::seconds>(date);
+[[nodiscard]] weatherer::util::DateTimeInfo
+weatherer::util::Time::GetCurrentDate() {
+  const std::chrono::time_point now = std::chrono::system_clock::now();
+  const std::chrono::time_point date =
+      std::chrono::floor<std::chrono::days>(now);
+  const std::chrono::time_point midnight =
+      std::chrono::time_point_cast<std::chrono::seconds>(date);
 
-	const std::time_t time = std::chrono::system_clock::to_time_t(midnight);
-	std::tm tm{};
+  const std::time_t time = std::chrono::system_clock::to_time_t(midnight);
+  std::tm tm{};
 
-	localtime_s(&tm, &time);
+  localtime_s(&tm, &time);
 
-	std::array<char, 80> buffer{};
-	if (strftime(buffer.data(), 80, "%Y-%m-%d", &tm) <= 0) {
-		throw std::runtime_error("An error has occurred whilst formatting the date.");
-	}
-	return DateTimeInfo{{buffer.data()}, time};
+  std::array<char, 80> buffer{};
+  if (strftime(buffer.data(), 80, "%Y-%m-%d", &tm) <= 0) {
+    throw std::runtime_error(
+        "An error has occurred whilst formatting the date.");
+  }
+  return DateTimeInfo{{buffer.data()}, time};
 }
 
-[[nodiscard]]
-weatherer::util::DateTimeInfo weatherer::util::Time::get_past_date(std::uint32_t days) {
-	if (days == 0) {
-		return get_current_date();
-	}
-	const std::chrono::time_point now = std::chrono::system_clock::now();
-	const std::chrono::time_point date = std::chrono::floor<std::chrono::days>(now);
-	const std::chrono::time_point midnight = std::chrono::time_point_cast<std::chrono::seconds>(
-		date - std::chrono::days(days));
+[[nodiscard]] weatherer::util::DateTimeInfo weatherer::util::Time::GetPastDate(
+    std::uint32_t days) {
+  if (days == 0) {
+    return GetCurrentDate();
+  }
+  const std::chrono::time_point now = std::chrono::system_clock::now();
+  const std::chrono::time_point date =
+      std::chrono::floor<std::chrono::days>(now);
+  const std::chrono::time_point midnight =
+      std::chrono::time_point_cast<std::chrono::seconds>(
+          date - std::chrono::days(days));
 
-	std::time_t time = std::chrono::system_clock::to_time_t(midnight);
-	std::tm tm{};
+  std::time_t time = std::chrono::system_clock::to_time_t(midnight);
+  std::tm tm{};
 
-	localtime_s(&tm, &time);
+  localtime_s(&tm, &time);
 
-	std::array<char, 80> buffer{};
-	if (strftime(buffer.data(), 80, "%Y-%m-%d", &tm) <= 0) {
-		throw std::runtime_error("An error has occurred whilst formatting the date.");
-	}
-	return DateTimeInfo{{buffer.data()}, time};
+  std::array<char, 80> buffer{};
+  if (strftime(buffer.data(), 80, "%Y-%m-%d", &tm) <= 0) {
+    throw std::runtime_error(
+        "An error has occurred whilst formatting the date.");
+  }
+  return DateTimeInfo{{buffer.data()}, time};
 }
 
-weatherer::util::DateTimeInfo weatherer::util::Time::get_past_date(const std::string& start_date, std::uint32_t days) {
-	const std::chrono::time_point init_date = parse_date(start_date);
-	const std::chrono::time_point date = std::chrono::floor<std::chrono::days>(init_date);
-	const std::chrono::time_point midnight = std::chrono::time_point_cast<std::chrono::seconds>(
-		date - std::chrono::days(days));
+weatherer::util::DateTimeInfo weatherer::util::Time::GetFutureDate(
+    const std::string& start_date, std::uint32_t days) {
+  const std::chrono::time_point init_date = ParseDate(start_date);
+  const std::chrono::time_point date =
+      std::chrono::floor<std::chrono::days>(init_date);
+  const std::chrono::time_point midnight =
+      std::chrono::time_point_cast<std::chrono::seconds>(
+          date + std::chrono::days(days));
 
-	const std::time_t time = std::chrono::system_clock::to_time_t(midnight);
-	std::tm tm;
+  const std::time_t time = std::chrono::system_clock::to_time_t(midnight);
+  std::tm tm;
 
-	localtime_s(&tm, &time);
+  localtime_s(&tm, &time);
 
-	std::array<char, 80> buffer{};
-	if (strftime(buffer.data(), 80, "%Y-%m-%d", &tm) <= 0) {
-		throw std::runtime_error("An error has occurred whilst formatting the date.");
-	}
-	return DateTimeInfo{{buffer.data()}, time};
+  std::array<char, 80> buffer{};
+  if (strftime(buffer.data(), 80, "%Y-%m-%d", &tm) <= 0) {
+    throw std::runtime_error(
+        "An error has occurred whilst formatting the date.");
+  }
+  return DateTimeInfo{{buffer.data()}, time};
 }
 
-[[nodiscard]]
-weatherer::util::DateTimeInfo weatherer::util::Time::get_future_date(std::uint32_t days) {
-	if (days == 0) {
-		return get_current_date();
-	}
-	const std::chrono::time_point now = std::chrono::system_clock::now();
-	const std::chrono::time_point date = std::chrono::floor<std::chrono::days>(now);
-	const std::chrono::time_point midnight = std::chrono::time_point_cast<std::chrono::seconds>(
-		date + std::chrono::days(days));
-
-	const std::time_t time = std::chrono::system_clock::to_time_t(midnight);
-	std::tm tm;
-
-	localtime_s(&tm, &time);
-
-	std::array<char, 80> buffer{};
-	if (strftime(buffer.data(), 80, "%Y-%m-%d", &tm) <= 0) {
-		throw std::runtime_error("An error has occurred whilst formatting the date.");
-	}
-	return DateTimeInfo{{buffer.data()}, time};
+[[nodiscard]] std::chrono::system_clock::time_point
+weatherer::util::Time::ParseDate(const std::string& date) {
+  std::tm tm{};
+  std::istringstream ss{date};
+  ss >> std::get_time(&tm, "%Y-%m-%d");
+  return std::chrono::system_clock::from_time_t(std::mktime(&tm));
 }
 
-weatherer::util::DateTimeInfo
-weatherer::util::Time::get_future_date(const std::string& start_date, std::uint32_t days) {
-	const std::chrono::time_point init_date = parse_date(start_date);
-	const std::chrono::time_point date = std::chrono::floor<std::chrono::days>(init_date);
-	const std::chrono::time_point midnight = std::chrono::time_point_cast<std::chrono::seconds>(
-		date + std::chrono::days(days));
-
-	const std::time_t time = std::chrono::system_clock::to_time_t(midnight);
-	std::tm tm;
-
-	localtime_s(&tm, &time);
-
-	std::array<char, 80> buffer{};
-	if (strftime(buffer.data(), 80, "%Y-%m-%d", &tm) <= 0) {
-		throw std::runtime_error("An error has occurred whilst formatting the date.");
-	}
-	return DateTimeInfo{{buffer.data()}, time};
+[[nodiscard]] int weatherer::util::Time::DifferenceInDays(
+    const std::string& first, const std::string& second) {
+  const std::chrono::system_clock::time_point start_date = ParseDate(first);
+  const std::chrono::system_clock::time_point end_date = ParseDate(second);
+  const auto duration = start_date - end_date;
+  return std::abs(
+      std::chrono::duration_cast<std::chrono::days>(duration).count());
 }
 
-[[nodiscard]]
-std::chrono::system_clock::time_point weatherer::util::Time::parse_date(const std::string& date) {
-	std::tm tm{};
-	std::istringstream ss{date};
-	ss >> std::get_time(&tm, "%Y-%m-%d");
-	return std::chrono::system_clock::from_time_t(std::mktime(&tm));
+std::chrono::system_clock::time_point weatherer::util::Time::ParseTime(
+    const std::string& time) {
+  std::tm time_tm = {};
+  std::istringstream ss(time);
+  ss >> std::get_time(&time_tm, "%Y-%m-%dT%H:%M");
+
+  if (ss.fail()) {
+    throw std::runtime_error("Failed to parse the first time string.");
+  }
+
+  const std::chrono::system_clock::time_point time_point =
+      std::chrono::system_clock::from_time_t(std::mktime(&time_tm));
+
+  return time_point;
 }
 
-[[nodiscard]]
-int weatherer::util::Time::difference_in_days(const std::string& first, const std::string& second) {
-	const std::chrono::system_clock::time_point start_date = parse_date(first);
-	const std::chrono::system_clock::time_point end_date = parse_date(second);
-	const auto duration = start_date - end_date;
-	return std::abs(std::chrono::duration_cast<std::chrono::days>(duration).count());
+std::tm weatherer::util::Time::ConvertTimePointToTm(
+    const std::chrono::system_clock::time_point& time_point) {
+  const std::time_t time = std::chrono::system_clock::to_time_t(time_point);
+  std::tm time_info;
+  localtime_s(
+      &time_info,
+      &time);  // Use localtime_s for safer conversion, especially on Windows
+
+  return time_info;
 }
--- src/util/Time.hpp
+++ src/util/Time.hpp
@@ -1,37 +1,35 @@
 #pragma once
-#include <string>
 #include <chrono>
+#include <string>
 
 namespace weatherer::util {
 
-	struct DateTimeInfo {
-		const std::string time_;
-		const std::time_t val_;
+struct DateTimeInfo {
+  std::string time_;
+  std::time_t val_;
+};
 
-		DateTimeInfo(std::string time, const std::time_t val) : time_{std::move(time)}, val_{val} {
-		}
+struct TimeFrame {
+  std::string start_date_;
+  std::string end_date_;
+};
 
-		~DateTimeInfo() = default;
-	};
+class Time {
+ public:
+  Time() = delete;
+  ~Time() = delete;
 
-	class Time {
-	public:
-		Time() = delete;
-		~Time() = delete;
-
-		[[nodiscard]]
-		static DateTimeInfo get_current_date();
-		[[nodiscard]]
-		static DateTimeInfo get_past_date(std::uint32_t days);
-		[[nodiscard]]
-		static DateTimeInfo get_past_date(const std::string& start_date, std::uint32_t days);
-		[[nodiscard]]
-		static DateTimeInfo get_future_date(std::uint32_t days);
-		[[nodiscard]]
-		static DateTimeInfo get_future_date(const std::string& start_date, std::uint32_t days);
-		[[nodiscard]]
-		static int difference_in_days(const std::string& first, const std::string& second);
-		[[nodiscard]]
-		static std::chrono::system_clock::time_point parse_date(const std::string& date);
-	};
-}
+  [[nodiscard]] static DateTimeInfo GetCurrentDate();
+  [[nodiscard]] static DateTimeInfo GetPastDate(std::uint32_t days);
+  [[nodiscard]] static DateTimeInfo GetFutureDate(const std::string& start_date,
+                                                  std::uint32_t days);
+  [[nodiscard]] static int DifferenceInDays(const std::string& first,
+                                            const std::string& second);
+  [[nodiscard]] static std::chrono::system_clock::time_point ParseDate(
+      const std::string& date);
+  [[nodiscard]] static std::chrono::system_clock::time_point ParseTime(
+      const std::string& time);
+  [[nodiscard]] static std::tm ConvertTimePointToTm(
+      const std::chrono::system_clock::time_point& time_point);
+};
+}  // namespace weatherer::util
