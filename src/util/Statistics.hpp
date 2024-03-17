c97834f0182331d884256423c87ca6d650524fa8
OneCheetah <91170949+OneCheetah@users.noreply.github.com>
2024-03-16 21:54:45 -0600

    Removed Median function

--- src/util/Statistics.hpp
+++ src/util/Statistics.hpp
@@ -1,25 +1,11 @@
 #pragma once
-#include <algorithm>
 #include <numeric>
-#include <ranges>
 #include <span>
 #include <type_traits>
 
 namespace weatherer::util {
 class Statistics {
- private:
-  static constexpr uint32_t kPlusInf_ = ~((1 << 23) - 1) - ((1 << 23) - 1);
-
-  static float ConvertToFloat(uint32_t n) {
-    n += ((1 << 23) - 1);
-    if (n & (1 << 31))
-      n ^= (1 << 31);
-    else
-      n = ~n;
-    return std::bit_cast<float>(n);
-  }
-
- public:
+public:
   Statistics() = delete;
   ~Statistics() = delete;
 
@@ -27,52 +13,6 @@ class Statistics {
             typename = std::enable_if<std::is_arithmetic_v<Ty_>>>
   [[nodiscard]] static constexpr double Mean(std::span<Ty_, Amt_> data) {
     return std::accumulate(data.begin(), data.end(), 0.0) / data.size();
-  }
-
-  template <typename Ty_, size_t Amt_,
-            typename = std::enable_if<std::is_arithmetic_v<Ty_>>>
-  [[nodiscard]] static constexpr double Median(std::span<Ty_, Amt_> data) {
-    if constexpr (std::is_integral_v<Ty_>) {
-      auto indices = std::views::iota(0u, ~0u);
-      auto counts =
-          indices | std::views::transform([&](const uint32_t partition) {
-            return std::ranges::count_if(
-                data, [&](auto i) { return i <= partition; });
-          });
-      if (data.size() % 2 == 0) {
-        auto lower_bound =
-            std::ranges::lower_bound(counts, (1 + data.size()) / 2) -
-            counts.begin();
-        auto upper_bound =
-            std::ranges::upper_bound(counts, (1 + data.size()) / 2) -
-            counts.begin();
-        return (lower_bound + upper_bound) / 2.0;
-      }
-      auto b = std::ranges::lower_bound(counts, (1 + data.size()) / 2);
-      return b - counts.begin();
-    } else if constexpr (std::is_floating_point_v<Ty_>) {
-      auto indices = std::views::iota(0u, kPlusInf_ + 1);
-      auto counts =
-          indices | std::views::transform([&](const uint32_t partition) {
-            return std::ranges::count_if(
-                data, [&](float i) { return i <= ConvertToFloat(partition); });
-          });
-      if (data.size() % 2 == 0) {
-        auto lower_bound =
-            std::ranges::lower_bound(counts, (1 + data.size()) / 2) -
-            counts.begin();
-        auto upper_bound =
-            std::ranges::upper_bound(counts, (1 + data.size()) / 2) -
-            counts.begin();
-        return ConvertToFloat((lower_bound + upper_bound) / 2.0);
-      }
-      auto b = std::ranges::lower_bound(counts, (data.size() + 1) / 2);
-      return ConvertToFloat(b - counts.begin());
-    } else {
-      static_assert(std::is_arithmetic_v<Ty_>,
-                    "Ty_ must be an arithmetic type");
-      return {};
-    }
   }
 };
 }  // namespace weatherer::util
